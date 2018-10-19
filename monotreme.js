3448d2b6e9ef152a268c69fece923d11bdfd1217
Chris Pearson <chrispsn@users.noreply.github.com>
2018-10-20 07:20:50 +1100

    Brought back render method

--- monotreme.js
+++ monotreme.js
@@ -9,14 +9,17 @@ const Monotreme = {
     },
     load: function() {
         this.data = this.IO.load(this.path);
-        this.node.value = this.data;
-        this.node.setAttribute("placeholder", this.data);
-        this.node.disabled = true;
+        this.render();
     },
     save: function() {
         this.data = this.node.value; // TODO What if we need to read the data into a JS equivalent like a {}, not just text?
         this.IO.save(this.path, this.data);
-        this.load();
+        this.render(); // TODO how to check that saved properly?
+    },
+    render: function() {
+        this.node.value = this.data;
+        this.node.setAttribute("placeholder", this.data);
+        this.node.disabled = true;
     },
     edit: function() {
         this.node.disabled = false;
@@ -27,15 +30,15 @@ const Monotreme = {
 // https://docs.microsoft.com/en-us/office/vba/language/reference/user-interface-help/opentextfile-method
 // https://www.w3schools.com/asp/asp_ref_filesystem.asp
 const IO_activex = {
-    fso: new ActiveXObject("Scripting.FileSystemObject"),
+    _fso: new ActiveXObject("Scripting.FileSystemObject"),
     load: function (filepath) {
-        var file = this.fso.OpenTextFile(filepath, 1);
+        var file = this._fso.OpenTextFile(filepath, 1);
         var content = file.ReadAll();
         file.Close();
         return content;
     },
     save: function (filepath, text) {
-        var file = this.fso.OpenTextFile(filepath, 2);
+        var file = this._fso.OpenTextFile(filepath, 2);
         file.write(text)
         file.Close();
     }
